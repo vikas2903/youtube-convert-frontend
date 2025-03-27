d72ac21cfd40f454d2f6f0258ef00630a17d4afe
Vikas Prasad <76564198+vikas2903@users.noreply.github.com>
2025-03-27 16:01:09 +0530

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
 
   // Backend API URL https://powerful-goofy-ketchup.glitch.me/
   // const BACKEND_URL = "https://youtube-converter-backend-2wvy.onrender.com";
-   const BACKEND_URL = "https://powerful-goofy-ketchup.glitch.me/";
+   const BACKEND_URL = "https://youtube-backend-01.onrender.com";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -114,7 +114,7 @@ function Home() {
           3. Once the conversion completes, click the "Download" button.
         </p>
         <p>Note: Maximum supported video length is 60 minutes.</p>
-        <a href ="https://vikasprasad.in.net">Vikas Prasad</a>
+        <a href ="https://vikasprasad.in.net">Vikas Prasad 01</a>
       </div>
     </div>
   );
